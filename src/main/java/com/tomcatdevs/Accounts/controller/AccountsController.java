256e17db6f3b5d19fb60d3d05c06d06ba5d219c9
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-10-30 22:21:24 +0530

    update response message changed

--- src/main/java/com/tomcatdevs/Accounts/controller/AccountsController.java
+++ src/main/java/com/tomcatdevs/Accounts/controller/AccountsController.java
@@ -41,7 +41,7 @@ public class AccountsController {
         boolean isUpdated = iAccountsService.updateCustomerAccountDetails(customerDto);
         if(isUpdated){
             return ResponseEntity.status(HttpStatus.ACCEPTED)
-                    .body(new ResponseDto(HttpStatus.ACCEPTED.toString(),"randii varnita makrariya"));
+                    .body(new ResponseDto(HttpStatus.ACCEPTED.toString(),"account updated successfully"));
         }else{
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(HttpStatus.EXPECTATION_FAILED.toString(),"customer account details updation failed"));
