347919542639143bf0c5112aab1576b64493a6a3
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-10-30 22:52:30 +0530

    added jakarta validation global error response handler logic

--- src/main/java/com/tomcatdevs/Accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tomcatdevs/Accounts/exception/GlobalExceptionHandler.java
@@ -1,19 +1,43 @@
 package com.tomcatdevs.Accounts.exception;
 
 import com.tomcatdevs.Accounts.dto.ErrorResponseDto;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.security.auth.login.AccountNotFoundException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    // HANDLE GLOBAL RUNTIME EXCEPTIION
+    // HANDLE JAKARTA BEAN ERROR RESPONSE
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        HashMap<String, String> validationErrorResponse = new HashMap<>();
+        List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+        errorList.forEach(error -> {
+            String errorField=error.toString();
+            String errorMessage = error.getDefaultMessage();
+            validationErrorResponse.put(errorField, errorMessage);
+        });
+        return new ResponseEntity<>(validationErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    // HANDLE GLOBAL RUNTIME EXCEPTION
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalErrorException(Exception ex, WebRequest webRequest) {
         return ResponseEntity
