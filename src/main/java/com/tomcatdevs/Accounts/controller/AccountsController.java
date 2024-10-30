2a8e5ce3677c177e9413a915419d454e60a722f9
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-10-30 21:19:41 +0530

    change message

--- src/main/java/com/tomcatdevs/Accounts/controller/AccountsController.java
+++ src/main/java/com/tomcatdevs/Accounts/controller/AccountsController.java
@@ -26,7 +26,7 @@ public class AccountsController {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new ResponseDto(HttpStatus.CREATED.toString(),"account created"));
+                .body(new ResponseDto(HttpStatus.CREATED.toString(),"account created successfuly"));
     }
 
     @GetMapping(value = "/fetch")
