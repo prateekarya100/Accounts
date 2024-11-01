3975d7af5ab2bd0ec2a188f19b180be831816330
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-11-01 21:30:41 +0530

    changes in delete resource response

--- src/main/java/com/tomcatdevs/Accounts/AccountsApplication.java
+++ src/main/java/com/tomcatdevs/Accounts/AccountsApplication.java
@@ -2,9 +2,11 @@ package com.tomcatdevs.Accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "AuditAwareImpl")
 public class AccountsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
